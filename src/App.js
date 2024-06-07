fa040f292589aebf754f7b5b2b7343d832979743
Osián <149536063+OsianJL@users.noreply.github.com>
2024-06-07 11:21:52 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const [text, setText] = useState("");
---
+++ src/testing.js
@@ -0,0 +1,9 @@
+// Función para sumar dos números
+function sumar(a, b) {
+    return a + c; // Error intencional: 'c' no está definido
+}
+
+// Llamada a la función
+let resultado = sumar(5, 3);
+
+console.log("El resultado es: " + resultado);
