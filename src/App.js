881f49f275b145be8048d1dc296e833353548a68
Osián <149536063+OsianJL@users.noreply.github.com>
2024-06-07 10:06:27 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-import { useState, useEffect } from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-const [text, setText] = useState("");
+  const [text, setText] = useState("");
 
-const handleChange = (event) => {
-  setText(event.target.value)
-}
-
-
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <input onChange={handleChange} value={text} />
+          <input onChange={handleChange} value={text} />
         </p>
-        <p style={{color: "white"}}>
-         texto: {text}
-        </p>
-       
+        <p style={{ color: "white" }}>texto: {text}</p>
       </header>
     </div>
   );
