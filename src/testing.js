270a52f90eedf69187301bfa90d0bf60cf9567c6
Osián <149536063+OsianJL@users.noreply.github.com>
2024-06-07 11:23:07 +0200

    erased file

--- src/testing.js
+++
@@ -1,9 +0,0 @@
-// Función para sumar dos números
-function sumar(a, b) {
-    return a + c; // Error intencional: 'c' no está definido
-}
-
-// Llamada a la función
-let resultado = sumar(5, 3);
-
-console.log("El resultado es: " + resultado);
